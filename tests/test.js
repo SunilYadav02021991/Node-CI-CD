c82077ba0ae757c62ff1058f243159b579bb92ac
Sunil Yadav <sunilyadav02021991@gmail.com>
2025-02-10 21:38:45 +0530

    added scripts

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,9 @@
 var request = require("supertest");
 var app = require("../app.js");
+var mocha = require("mocha");
+var describe = mocha.describe;
+var it = mocha.it;
+
 describe("GET /", function() {
  it("respond with hello world", function(done) {
  //navigate to root and check the the response is "hello world"
