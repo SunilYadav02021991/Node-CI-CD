fe487c35bfd245fa85f5d1e1489438842c434ea1
Sunil Yadav <sunilyadav02021991@gmail.com>
2025-02-10 21:40:39 +0530

    Added tests

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ var it = mocha.it;
 
 describe("GET /", function() {
  it("respond with hello world", function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get("/").expect("hello world", done);
+    //navigate to root and check the the response is "hello world"
+    request(app).get("/").expect("hello world", done);
  });
 });
